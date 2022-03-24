8330b797a684fece029736d37d4ca7a575cf158c
borksson <elcapitan.borkson@gmail.com>
2022-03-24 15:59:44 -0600

    Robot position can be changed by keys, pause bool added

--- src/Game.js
+++ src/Game.js
@@ -6,24 +6,67 @@ constructor(props) {
     super(props);
     this.state = {
         activeRbtTop: 0,
-        activeRbtLeft: 0
+        activeRbtLeft: 0,
+        isPaused: true
     }
     this.LOG_TAG = 'GAME: '
 }
 
-moveRight = () => {
-    this.setState({activeRbtLeft: this.state.activeRbtLeft + 10})
+pause = () => {
+    if(this.state.isPaused){
+        this.setState({isPaused:false})
+    } else {
+        this.setState({isPaused:true})
+    }
+}
+
+
+keyCapture = (event) => {
+    //TODO: Add diagonal movement
+    switch (event.key){
+        case 'w':
+            this.setState({activeRbtTop: this.state.activeRbtTop - 5})
+            return;
+        case 's':
+            this.setState({activeRbtTop: this.state.activeRbtTop + 5})
+            return;
+        case 'a':
+            this.setState({activeRbtLeft: this.state.activeRbtLeft - 5})
+            return;
+        case 'd':
+            this.setState({activeRbtLeft: this.state.activeRbtLeft + 5})
+            return;
+        default:
+            return;
+    }
+}
+
+componentDidMount(){
+    document.addEventListener("keydown", this.keyCapture, false);
+}
+componentWillUnmount(){
+    document.removeEventListener("keydown", this.keyCapture, false);
 }
 
 render() {
+    let pause
+    if(this.state.isPaused){
+        pause = <label>yes</label>
+    }else {
+        pause = <label>no</label>
+    }
+
     return (
         <main className="Game">
-            <div>The game is working.</div>
-            <button onClick={this.moveRight}>Move Right</button>
+            <div>
+                The game is working. Is the game paused? {pause}
+            </div>
+            <button onClick={this.pause}>Pause</button>
             <Robot top={this.state.activeRbtTop} left={this.state.activeRbtLeft}/>
         </main>
     )
 }
+
 }
 
 export default Game
--- src/Robot.js
+++ src/Robot.js
@@ -2,6 +2,7 @@ import "./Robot.css";
 
 export default function Robot(props) {
     return (
+        //TODO: Change to hold state?
         <div className="rbt" style={{top:props.top+'px', left:props.left+'px', position:'absolute'}}/>
     )
 }
