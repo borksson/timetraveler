f1c6a57d085fbf747cb2cba459a877c820374caa
borksson <elcapitan.borkson@gmail.com>
2022-03-24 16:09:04 -0600

    Created moving robot class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import {Component} from "react";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <main className="App">
+                <label>The App is running.</label>
 
+            </main>
+        )
+    }
 }
 
 export default App
---
+++ src/Robot.js
@@ -0,0 +1,13 @@
+import {Component} from "react";
+
+class Robot extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
